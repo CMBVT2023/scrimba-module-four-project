4313705f8e27253ee3c6e167cec7a529f0b9b910
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-11-05 11:21:34 -0800

    Added a dark mode toggle to the fun facts project

--- src/FunFactProject/FunFactsPage.jsx
+++ src/FunFactProject/FunFactsPage.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavBar } from "./components/NavBar";
 import { Main } from "./components/Main";
 import "./FunFactsStyles.css"
 
 export function FunFactsPage() {
+    const [ darkMode, setDarkMode ] = useState(false);
+
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
 
     return (
         <div className="container">
-            <NavBar />
-            <Main />
+            <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Main darkMode={darkMode} />
         </div>
     )
 }
