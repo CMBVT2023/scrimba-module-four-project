de3ea4915231b54f352f89cce2fb871eb7008e59
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-11-06 10:09:47 -0800

    Added note titles to each note

--- src/notes-project/components/Sidebar.jsx
+++ src/notes-project/components/Sidebar.jsx
@@ -3,18 +3,25 @@ import React from "react";
 import styles from '../NotesStyles.module.css'
 
 export function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                className={`${styles.title} ${
-                    note.id === props.currentNote.id ? styles.selectedNote : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className={styles.textSnippet}>Note {index + 1}</h4>
+    const noteElements = props.notes.map((note, index) => {
+        let noteString = JSON.stringify(note.body)
+        noteString = noteString.substring(1, noteString.length - 1)
+        
+        let noteTitle = noteString.split('\\n')[0]
+
+        return (
+            <div key={note.id}>
+                <div
+                    className={`${styles.title} ${
+                        note.id === props.currentNote.id ? styles.selectedNote : ""
+                    }`}
+                    onClick={() => props.setCurrentNoteId(note.id)}
+                >
+                    <h4 className={styles.textSnippet}>{noteTitle}</h4>
+                </div>
             </div>
-        </div>
-    ))
+        )
+    })
 
     return (
         <section className={`${styles.pane} ${styles.sidebar}`}>
