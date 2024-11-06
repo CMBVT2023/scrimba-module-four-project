fa8f28afbfb7094283cdd7c6409038067a2dca59
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-11-06 10:51:20 -0800

    Updated the notes rearranging code to match the tutorial

--- src/notes-project/NotesPage.jsx
+++ src/notes-project/NotesPage.jsx
@@ -33,7 +33,23 @@ export function NotesPage() {
     }
     
     function updateNote(text) {
+        //? Solution using video pseudocode
         setNotes(oldNotes => {
+            let newNotes = [];
+
+            for (const note of oldNotes) {
+                if (note.id === currentNoteId) {
+                    newNotes.unshift({...note, body: text});
+                } else {
+                    newNotes.push(note)
+                }
+            }
+
+            return newNotes;
+        })
+        
+        //? My solution to rearranging the notes.
+        /* setNotes(oldNotes => {
             let oldIndex;
             let newNotes = [...oldNotes].filter((oldNote, index) => {
                 if (oldNote.id != currentNoteId) return true;
@@ -41,18 +57,28 @@ export function NotesPage() {
                 return false;
             });
             return [{...oldNotes[oldIndex], body: text}, ...newNotes]
+        }) */
 
-            
-            // let noteIndex;
-            // let newArr = oldNotes.map((oldNote, index) => {
-            //     if (oldNote.id === currentNoteId) noteIndex = index;
-            //     return oldNote.id === currentNoteId
-            //         ? { ...oldNote, body: text }
-            //         : oldNote
-            // })
-            // newArr.unshift(newArr.splice(noteIndex, 1)[0]);
-            // return newArr;
-        })
+        //? First attempt at rearranging notes.
+        /* setNotes(oldNotes => {
+            let noteIndex;
+            let newArr = oldNotes.map((oldNote, index) => {
+                if (oldNote.id === currentNoteId) noteIndex = index;
+                return oldNote.id === currentNoteId
+                    ? { ...oldNote, body: text }
+                    : oldNote
+            })
+            newArr.unshift(newArr.splice(noteIndex, 1)[0]);
+            return newArr;
+        }) */
+        
+        
+        //! Old code that does not rearrange the array
+        /* setNotes(oldNotes => oldNotes.map(oldNote => {
+            return oldNote.id === currentNoteId
+                ? { ...oldNote, body: text }
+                : oldNote
+        })) */
     }
     
     function findCurrentNote() {
