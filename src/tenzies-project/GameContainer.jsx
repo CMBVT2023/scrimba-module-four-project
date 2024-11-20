a9c0cedf3c487bbd48f9096399a876b105163180
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-11-20 14:50:23 -0800

    Reworked the die array to now store objects containing the die value and a holding property. Also switched to using a for loop to populate the array like in the tutorial

--- src/tenzies-project/GameContainer.jsx
+++ src/tenzies-project/GameContainer.jsx
@@ -5,22 +5,33 @@ export function GameContainer({styles}) {
     const [ dice, setDice ] = useState(allNewDice())
 
     /**
-     * Challenge: Create a `Roll Dice` button that will re-roll
-     * all 10 dice
+     * Challenge: Update the array of numbers in state to be
+     * an array of objects instead. Each object should look like:
+     * { value: <random number>, isHeld: false }
      * 
-     * Clicking the button should generate a new array of numbers
-     * and set the `dice` state to that new array (thus re-rendering
-     * the array to the page)
+     * Making this change will break parts of our code, so make
+     * sure to update things so we're back to a working state
      */
 
     function allNewDice() {
-        return Array(10).fill(1).map(value => Math.ceil(Math.random() * 6));
+        //! My way of originally filling the array
+        // const newDice = new Array(10).fill(1).map(value => Math.ceil(Math.random() * 6));
+
+        const newDice = [];
+        for (let i = 0; i < 10; i++) {
+            newDice[i] = {
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false
+            }
+        }
+
+        return newDice
     }
 
     return (
         <main className={`${styles.displayFlexCenter} ${styles.gameElement}`}>
             <div className={styles.diceContainer}>
-                {dice.map((value, index) => <Die key={`die-${index + 1}`} value={value} styles={styles} />)}
+                {dice.map((die, index) => <Die key={`die-${index + 1}`} value={die.value} styles={styles} />)}
             </div>
             <button onClick={() => setDice(allNewDice())} className={styles.reRollButton}>Roll</button>
         </main>
