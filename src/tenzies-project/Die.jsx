369176596102dd50c64784c187d2aa64c174b831
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-11-22 09:27:20 -0800

    Added the styling to signify if a die component's value is being held

--- src/tenzies-project/Die.jsx
+++ src/tenzies-project/Die.jsx
@@ -1,7 +1,8 @@
-export function Die({styles, value}) {
+export function Die({styles, value, isHeld}) {
 
     return (
-        <div className={`${styles.displayFlexCenter} ${styles.dieElement}`}>
+        <div className={`${styles.displayFlexCenter} ${styles.dieElement}`} 
+        style={{backgroundColor: `${isHeld ? '#59E391' : '#FFFFFF'}`}}>
             <p>{value}</p>
         </div>
     )
--- src/tenzies-project/GameContainer.jsx
+++ src/tenzies-project/GameContainer.jsx
@@ -3,17 +3,7 @@ import { Die } from "./Die";
 
 export function GameContainer({styles}) {
     const [ dice, setDice ] = useState(allNewDice())
-    let id = useId()
-
-    /**
-     * Challenge: Update the array of numbers in state to be
-     * an array of objects instead. Each object should look like:
-     * { value: <random number>, isHeld: false }
-     * 
-     * Making this change will break parts of our code, so make
-     * sure to update things so we're back to a working state
-     */
-
+    
     function allNewDice() {
         //! My way of originally filling the array
         // const newDice = new Array(10).fill(1).map(value => Math.ceil(Math.random() * 6));
@@ -33,7 +23,7 @@ export function GameContainer({styles}) {
     return (
         <main className={`${styles.displayFlexCenter} ${styles.gameElement}`}>
             <div className={styles.diceContainer}>
-                {dice.map((die) => <Die key={die.id} value={die.value} styles={styles} />)}
+                {dice.map((die) => <Die key={die.id} value={die.value} styles={styles} isHeld={die.isHeld} />)}
             </div>
             <button onClick={() => setDice(allNewDice())} className={styles.reRollButton}>Roll</button>
         </main>
